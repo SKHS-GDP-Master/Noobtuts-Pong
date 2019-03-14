b80305cf409f5e57d8f4d92a1a570dbc2d9016b5
Douglas Urner <dlu@canishe.com>
2019-03-13 20:27:56 -0700

    Renaming.

---
+++ Assets/Scripts/PaddleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour {
+  public float speed = 30;
+  public string axis = " --- Select Input Axis --- ";
+
+  private Rigidbody2D rb;
+
+  private void Start() {
+    rb = GetComponent<Rigidbody2D>();
+  }
+
+  void FixedUpdate() {
+    float v = Input.GetAxisRaw(axis);
+    rb.velocity = new Vector2(0, v) * speed;
+  }
+}
